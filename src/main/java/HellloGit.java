8941e8f3ce51d97dbd669e93b96a8b500064b1c8
omerziauddin <omer.ansari@quicksoft.com>
2021-07-19 01:44:33 +0530

    from nrnch newBranchOmer

--- src/main/java/HellloGit.java
+++ src/main/java/HellloGit.java
@@ -3,7 +3,7 @@ public class HellloGit {
 
 	public static void main(String[] args) {
 		System.out.println("hi this is first commit");
-
+        System.out.println("from second branch newBranchOmer");
 	}
 
 }
